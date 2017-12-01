6f4ddf41685037702493b045a9f004cb5068449e
alan <alan@LAPTOP-QDKPLV6R>
2017-12-01 13:59:16 -0600

    este es el examen de la unidad 4

--- src/examenuni4/ExamenUni4.java
+++ src/examenuni4/ExamenUni4.java
@@ -3,46 +3,52 @@ import java.util.Scanner;
 public class ExamenUni4 {
 
 public static void main(String[] args) {
-        
-    }
     ArbolBinario abo = new ArbolBinario();
         int var;
         int valor;
         Scanner texto = new Scanner(System.in);
 
-        System.out.println(" ingresa los vaores  : ");
+        System.out.println(" ingresa los valores  : ");
         for (int i = 1; i <= 7; i++) {
-            
             valor = (int) (Math.random() * 100);
             System.out.print(valor + " ");
             abo.agregar(valor);
         }
 
         do {
+
+
             System.out.println(" ");
             System.out.println(" ");
             System.out.println("seleccione una opcion deceada ");
+
+
             System.out.println(" ");
             System.out.println(" ");
-            System.out.println("1.-  cuales son las Cantidad de nodos del arbol que estamos realizando atra vez de los arboles binario");
-            System.out.println("2.-La  Altura del  arbol es ");
+            System.out.println("1.- Cantidad de nodos del arbol");
+            System.out.println("2.- Altura del arbol");
             var = texto.nextInt();
             switch (var) {
                 case 1:
-                    System.out.println("Cantidad de nodos del árbol que estamos realizando :" + abo.cantidad());
+                    System.out.println("Cantidad de nodos del árbol:" + abo.cantidad());
                     break;
 
                 case 2:
-                    System.out.println(" su altura del arbol es:" + abo.retornarAltura());
+                    System.out.println("La alturra del arbol es:" + abo.retornarAltura());
                     break;
 
-                    
-                    
-                    
             }
-        }// while (var != 0);
+        } while (var != 0);
 
     }
+}
+
+
+        
+    
+    
+
+    
 
     
 
